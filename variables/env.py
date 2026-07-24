4a6db3dbfe458d9b80cca81f3792e272c2fd0f46
Rasheedat-Ajidagba95 <140977388+Rasheedat-Ajidagba95@users.noreply.github.com>
2026-07-24 22:01:36 +0100

    updated script

--- variables/env.py
+++ variables/env.py
@@ -1,7 +1,6 @@
 # Config for the test suite. Swap these per environment.
 
 UI_URL = "https://automationintesting.online/admin"
-API_URL = "https://automationintesting.online/api"
 
 ADMIN_USER = "admin"
 ADMIN_PASSWORD = "password"   
